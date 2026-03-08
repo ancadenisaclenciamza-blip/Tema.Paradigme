997e32fbc4aa5c5ccb80a8e7e4dd3b1bd293642e
Anca <ancadenisa.clenciamza@ulbsibiu.ro>
2026-03-08 15:57:52 +0200

    Tema Paradigme

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student student1= new Student(474,"Popa", "Alin", "A123" );
+        System.out.println(student1);
+
+    }
+}
---
+++ src/Student.java
@@ -0,0 +1,17 @@
+public class Student {
+    private Integer numarMatricol;
+    private String nume;
+    private String prenume;
+    private String formatieDeStudiu;
+
+    public Student(Integer numarMatricol, String nume,  String prenume, String formatieDeStudiu) {
+        this.numarMatricol = numarMatricol;
+        this.nume = nume;
+        this.prenume=prenume;
+        this.formatieDeStudiu=formatieDeStudiu;
+    }
+    @Override
+    public String toString() {
+        return "Student{" + "numarMatricol=" + numarMatricol + ", nume=" + nume + '}';
+    }
+}
